5f3639b2f8cd6951efc2ab1322cfdfc19442bcc1
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-05-27 02:16:10 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,9 @@
 def speak_to_grandma(say)
   if say == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
-  elsif say == (say.)
+  elsif say != (say.upcase)
+    puts "HUH?! Speak UP, SONNY!"
+  else
+      puts "NO, NOT SINCE 1938!"
 end
 end 
